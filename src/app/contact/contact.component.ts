b3207da6c3b4a1c701060bc47e4ae36161587803
Aishwarya <imaishwaryaprabhu@gmail.com>
2021-02-04 09:45:56 +0530

    contact form integrated

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  contactForm = new FormGroup({
+    name: new FormControl(null, [Validators.required, Validators.minLength(4), Validators.maxLength(50)]),
+    phoneNumber: new FormControl(null, [Validators.required, Validators.pattern('^[789][0-9]{9}')]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    message: new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(255)])
+  });
+
+  showMessage = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+      setTimeout(() => {
+        this.showMessage = !!this.contactForm.valid;
+        this.contactForm.reset();
+      }, 2000);
+  }
+
+  get name() {
+    return this.contactForm.get('name');
+  }
+
+  get phoneNumber() {
+    return this.contactForm.get('phoneNumber');
+  }
+
+  get email() {
+    return this.contactForm.get('email');
+  }
+
+  get message() {
+    return this.contactForm.get('message');
   }
 
 }
